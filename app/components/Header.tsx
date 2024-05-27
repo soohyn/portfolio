f031dd6de564f1b5305e8ec17623f58bb39a3005
sooheyon <sooheyonjo@gmail.com>
2024-05-27 22:12:48 +0900

    create header

---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <header className="flex flex-row layout py-3 px-2">
+      <h1 className="highlight text-2xl font-extrabold">Soohyn Portfolio</h1>
+      <nav></nav>
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
