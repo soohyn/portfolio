b8ba272b2dac506ae9604a04c27af9a10e545c07
sooheyon <sooheyonjo@gmail.com>
2024-05-28 15:17:32 +0900

    nav action

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,58 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useEffect, useState } from "react";
+import NavItem from "./NavItem";
+import { removeAllListeners } from "process";
+import Link from "next/link";
+
+export type SectionId = "about" | "tech" | "guest" | "projects";
+interface NavItemData {
+  label: string;
+  id: SectionId;
+}
+
+const navItemData: NavItemData[] = [
+  { label: "About", id: "about" },
+  { label: "Tech", id: "tech" },
+  { label: "Projects", id: "projects" },
+  { label: "Guest", id: "guest" },
+];
 
 const Header: FC = () => {
+  const [scrollY, setScrollY] = useState<number>(0);
+
+  const getScroll = () => {
+    const y = window.scrollY;
+    setScrollY(y);
+  };
+
+  useEffect(() => {
+    addEventListener("scroll", getScroll);
+
+    return () => {
+      removeAllListeners();
+    };
+  }, []);
+
+  const navItemMap = navItemData.map((data, idx) => {
+    return (
+      <NavItem
+        key={data.id}
+        label={data.label}
+        sectionId={data.id}
+        y={scrollY}
+      />
+    );
+  });
+
   return (
     <header className="bg-white fixed top-0 w-full">
-      <div className="layout flex flex-row w-full py-3 global-px items-center">
-        <h1 className="highlight text-2xl font-extrabold">Soohyn,</h1>
+      <div className="layout flex flex-row w-full py-3 global-px items-center select-none">
+        <Link href={"/"}>
+          <h1 className="highlight text-2xl font-extrabold">Soohyn,</h1>
+        </Link>
         <nav className="px-8">
-          <ul className="flex flex-row gap-4">
-            <li>About</li>
-            <li>Tech</li>
-            <li>Projects</li>
-            <li>Guest</li>
-          </ul>
+          <ul className="flex flex-row gap-4">{navItemMap}</ul>
         </nav>
         <div></div>
       </div>
---
+++ app/components/NavItem.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { FC, useEffect, useState } from "react";
+import { SectionId } from "./Header";
+
+interface NavItemProps {
+  label: string;
+  sectionId: SectionId;
+  y: number;
+}
+
+
+const NavItem: FC<NavItemProps> = ({ label, sectionId, y }) => {
+  const [selected, setSelected] = useState<boolean>(false);
+
+  const getSectionElementPosition = () => {
+    const el = document.querySelector("#" + sectionId + "-section");
+
+    if (!el) return;
+
+    const result = el.getBoundingClientRect();
+
+    return result;
+  };
+
+  const onClickNavItem = () => {
+    const result = getSectionElementPosition();
+
+    if (!result) return;
+
+    window.scrollBy(0, result.top);
+  };
+
+  const getItemSelected = (): void => {
+    const result = getSectionElementPosition();
+
+    if (!result) return;
+
+    const condition = window.innerHeight / 2;
+
+    if (result.top > condition || result.bottom < condition) {
+      setSelected(false);
+      return;
+    }
+
+    setSelected(true);
+  };
+
+  useEffect(() => {
+    getItemSelected();
+  }, [y]);
+
+  return (
+    <li className={`${selected && "--selected"}`}>
+      <button onClick={onClickNavItem}>{label}</button>
+    </li>
+  );
+};
+
+export default NavItem;
