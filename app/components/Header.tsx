c08d8f85737d72abe81fb67635a69c8ee5133072
sooheyon <sooheyonjo@gmail.com>
2024-05-28 10:34:14 +0900

    styled header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,8 +5,8 @@ const Header: FC = () => {
     <header className="bg-white fixed top-0 w-full">
       <div className="layout flex flex-row w-full py-3 global-px items-center">
         <h1 className="highlight text-2xl font-extrabold">Soohyn,</h1>
-        <nav>
-          <ul className="flex flex-row">
+        <nav className="px-8">
+          <ul className="flex flex-row gap-4">
             <li>About</li>
             <li>Tech</li>
             <li>Projects</li>
