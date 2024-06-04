6654b63e60ed5e6e5d311fb72d4016f57fc78132
sooheyon <sooheyonjo@gmail.com>
2024-06-04 19:30:30 +0900

    create Modal

--- app/components/GuestForm.tsx
+++ app/components/GuestForm.tsx
@@ -1,47 +1,60 @@
-"use client"
+"use client";
 import { FC, FormEvent, useState } from "react";
+import Modal from "./Modal";
 
 const GuestForm: FC = () => {
-  const [error,setError] = useState()
+  const [error, setError] = useState();
+  const [modalOpened, setModalOpened] = useState<boolean>(false);
 
-  const onSubmitForm = async (e:FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const data = new FormData(e.target as HTMLFormElement)
-    
-    const name = data.get('name')
-    const email = data.get('email')
-    const password = data.get('password')
-    const message = data.get('message')
+  const onSubmitForm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = new FormData(e.target as HTMLFormElement);
 
-    if(!(name && email && password && message)){
+    const name = data.get("name");
+    const email = data.get("email");
+    const password = data.get("password");
+    const message = data.get("message");
+
+    if (!(name && email && password && message)) {
       setError(error);
     }
 
-    try {
+    setModalOpened(true);
 
+    try {
     } catch (error) {
       console.error(error);
     }
   };
 
+  const onCloseModal =()=>{
+    setModalOpened(false);
+  }
+
   return (
-    <form onSubmit={onSubmitForm} className="flex flex-col p-5 sm:p-7 bg-amber-100/50 rounded-lg backdrop-blur-xl gap-3 w-full guest-form-inner-shadow ">
-      <input name="name" placeholder="name" className="" type="text" />
-      <input name="email" placeholder="email" className="" type="email" />
-      <input
-        name="password"
-        placeholder="password"
-        type="password"
-        className=""
-      />
-      <textarea
-        className=" resize-none"
-        placeholder="message"
-        rows={6}
-        maxLength={1000}
-      />
-      <input type="submit" value="submit" />
-    </form>
+    <>
+      <form
+        onSubmit={onSubmitForm}
+        className="flex flex-col p-5 sm:p-7 bg-amber-100/50 rounded-lg backdrop-blur-xl gap-3 w-full guest-form-inner-shadow "
+      >
+        <input name="name" placeholder="name" className="" type="text" />
+        <input name="email" placeholder="email" className="" type="email" />
+        <input
+          name="password"
+          placeholder="password"
+          type="password"
+          className=""
+        />
+        <textarea
+          className=" resize-none"
+          placeholder="message"
+          rows={6}
+          maxLength={1000}
+        />
+        <input type="submit" value="submit" />
+      </form>
+      <Modal isOpened={modalOpened} onClose={onCloseModal}/>
+    </>
   );
 };
 export default GuestForm;
---
+++ app/components/Modal.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { FC, HtmlHTMLAttributes, useEffect } from "react";
+
+interface ModalProps extends HtmlHTMLAttributes<HTMLDivElement> {
+  isOpened: boolean;
+  onClose: () => void;
+}
+
+const Modal: FC<ModalProps> = ({ isOpened, children, onClose }) => {
+  const setRootScroll = () => {
+    const root = document.querySelector("body") as HTMLElement;
+
+    if (!root) return;
+
+    if (isOpened) {
+      root.style.overflow = "hidden";
+    } else {
+      root.style.overflow = "auto";
+    }
+  };
+
+  const onClickClose = () => {
+    onClose();
+  };
+
+  useEffect(() => {
+    setRootScroll();
+  }, [isOpened]);
+
+  return (
+    <div
+      className={`${
+        isOpened ? "visible" : "invisible"
+      } backdrop-blur-lg flex justify-center items-center fixed top-0 bottom-0 left-0 right-0 z-10`}
+      onClick={onClickClose}
+    >
+      <div className="bg-white p-10">{children}</div>
+    </div>
+  );
+};
+
+export default Modal;
