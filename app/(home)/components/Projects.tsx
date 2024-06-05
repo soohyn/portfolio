b46e24bac8448d97690eadb20e05a4a8af2dfaa2
sooheyon <sooheyonjo@gmail.com>
2024-06-05 16:17:26 +0900

    add grid pattern to project section

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 
 const Projects: FC = () => {
   return (
-    <section id="projects-section" className="section-layout bg-gray-50">
+    <section id="projects-section" className="section-layout  pattern">
       <div className="flex flex-col layout items-center">
         <h2 className="section-header">
           <HighlightingText text="Project" />
