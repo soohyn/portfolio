ec6c20015ac3ae56f79fad95021e58bbf33cb51c
sooheyon <sooheyonjo@gmail.com>
2024-06-05 12:03:33 +0900

    create projectcard

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,7 +1,6 @@
 import HighlightingText from "@/app/components/HighlightingText";
-import Tag from "@/app/components/Tag";
+import ProjectCard from "@/app/components/ProjectCard";
 import { projects } from "@/public/config";
-import Image from "next/image";
 import { FC } from "react";
 
 const Projects: FC = () => {
@@ -12,47 +11,9 @@ const Projects: FC = () => {
           <HighlightingText text="Project" />
         </h2>
 
-        <ul className="flex flex-row flex-wrap w-full mt-20">
+        <ul className="grid md:grid-cols-2 w-full mt-20 gap-x-8 gap-y-16">
           {projects.map((p, idx) => {
-            return (
-              <li
-                key={`project-${idx}`}
-                className={`${idx % 2 === 0 ? "md:pr-4" : "md:pl-4"} pb-8  w-full md:w-[50%]`}
-              >
-                <div className="flex flex-col w-full overflow-hidden rounded-md bg-white inner-shadow">
-                  <div className="flex w-full h-[120px] relative bg-amber-100">
-                    <Image
-                      fill
-                      className="w-full h-full object-cover"
-                      objectFit="cover"
-                      src={p.thumbnail}
-                      alt={p.name}
-                    />
-                  </div>
-                  <div className="flex flex-col p-4 gap-2">
-                    <div className="flex flex-row items-center gap-2">
-                      <span className="text-lg font-bold">{p.name}</span>
-                      <Tag
-                        text={`${p.isTeam ? "팀" : "개인"}`}
-                        bgTheme={`${
-                          !p.isTeam ? "bg-gray-200" : "bg-amber-200"
-                        }`}
-                        textTheme={`${
-                          !p.isTeam ? "text-gray-600" : "text-amber-600"
-                        }`}
-                      />
-                    </div>
-                    <span>{p.description}</span>
-                    <span>{p.deploy}</span>
-                    <ul className="flex flex-row gap-2">
-                      {p.skills.map((v, idx) => {
-                        return <li key={`${p.name}-${idx}`}>{v}</li>;
-                      })}
-                    </ul>
-                  </div>
-                </div>
-              </li>
-            );
+            return <ProjectCard key={`project-${idx}`} project={p} idx={idx} />;
           })}
         </ul>
       </div>
---
+++ app/components/ProjectCard.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { FC } from "react";
+import Tag from "./Tag";
+import BorderTag from "./BorderTag";
+import { FiLink } from "react-icons/fi";
+
+interface ProjectCardProps {
+  project: Project;
+  idx: number;
+}
+
+// 포스트잍은 촌스러운거 같아...
+// 걍 박스 아니면 포스트잍...
+
+const ProjectCard: FC<ProjectCardProps> = ({ idx, project }) => {
+  return (
+    <li
+      className={`flex flex-col w-full bg-white rounded-lg inner-shadow overflow-hidden transition-all cursor-pointer active:scale-[98%] relative`}
+    >
+      <div className="flex w-full h-[120px] relative bg-amber-100">
+        <Image
+          fill
+          className="w-full h-full object-cover"
+          src={project.thumbnail}
+          alt={project.name}
+        />
+      </div>
+      <div className="flex flex-col p-4 gap-2">
+        <div className="flex flex-row items-center gap-2">
+          <span className="text-lg font-bold">{project.name}</span>
+          <Tag
+            text={`${project.isTeam ? "팀" : "개인"}`}
+            bgTheme={`${!project.isTeam ? "bg-gray-200" : "bg-amber-200"}`}
+            textTheme={`${
+              !project.isTeam ? "text-gray-600" : "text-amber-600"
+            }`}
+          />
+        </div>
+        <span>{project.description}</span>
+        <span className="text-sm text-gray-500">{project.deploy}</span>
+        <ul className="flex flex-row flex-wrap gap-2">
+          {project.skills.map((v, idx) => {
+            return <BorderTag key={`${project.name}-${idx}`} text={v} />;
+          })}
+        </ul>
+      </div>
+
+      <div className="flex justify-center items-center absolute top-0 left-0 right-0 bg-white bottom-0 opacity-0 hover:opacity-40 transition-all "></div>
+    </li>
+  );
+};
+
+export default ProjectCard;
