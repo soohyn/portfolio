96b8f7dfff7c72c2c4f35a40c32a0e9bd13369ce
soohyn <sooheyonjo@gmail.com>
2024-06-05 19:34:10 +0900

    move modal position

--- app/components/GuestForm.tsx
+++ app/components/GuestForm.tsx
@@ -33,32 +33,36 @@ const GuestForm: FC = () => {
   };
 
   return (
-    <div className=" bg-gray-100 shadow-xl rounded-md backdrop-blur-xl w-full inner-shadow overflow-hidden ">
-      <div className="flex flex-row bg-gray-300 w-full p-3 gap-3 items-center shadow-inner">
-        <MacDotButton color={"#fb4646"} />
-        <MacDotButton color={"#fcb024"} />
-        <MacDotButton color={"#28c132"} />
+    <>
+      <div className=" bg-gray-100 shadow-xl rounded-md backdrop-blur-xl w-full inner-shadow overflow-hidden ">
+        <div className="flex flex-row bg-gray-300 w-full p-3 gap-3 items-center shadow-inner">
+          <MacDotButton color={"#fb4646"} />
+          <MacDotButton color={"#fcb024"} />
+          <MacDotButton color={"#28c132"} />
+        </div>
+        <form
+          onSubmit={onSubmitForm}
+          className="p-3 sm:p-5 flex flex-col gap-3 w-full"
+        >
+          <input name="name" placeholder="name" className="" type="text" />
+          <input name="email" placeholder="email" className="" type="email" />
+          <input
+            name="password"
+            placeholder="password"
+            type="password"
+            className=""
+          />
+          <textarea
+            className=" resize-none"
+            placeholder="message"
+            rows={5}
+            maxLength={300}
+          />
+          <input type="submit" value="send" />
+        </form>
       </div>
-      <form onSubmit={onSubmitForm} className="p-3 sm:p-5 flex flex-col gap-3 w-full">
-        <input name="name" placeholder="name" className="" type="text" />
-        <input name="email" placeholder="email" className="" type="email" />
-        <input
-          name="password"
-          placeholder="password"
-          type="password"
-          className=""
-        />
-        <textarea
-          className=" resize-none"
-          placeholder="message"
-          rows={5}
-          maxLength={300}
-        />
-        <input type="submit" value="send" />
-      </form>
-
       <Modal isOpened={modalOpened} onClose={onCloseModal} />
-    </div>
+    </>
   );
 };
 export default GuestForm;
