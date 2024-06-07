cf20f4e1d1609d50a4106c3010a43348b4d2f762
soohyn <sooheyonjo@gmail.com>
2024-06-07 18:08:02 +0900

    modal style, responsible

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { FC, HtmlHTMLAttributes, useEffect } from "react";
+import { FiXCircle } from "react-icons/fi";
 
 interface ModalProps extends HtmlHTMLAttributes<HTMLDivElement> {
   isOpened: boolean;
@@ -31,10 +32,17 @@ const Modal: FC<ModalProps> = ({ isOpened, children, onClose }) => {
     <div
       className={`${
         isOpened ? "visible" : "invisible"
-      } backdrop-blur-md flex justify-center items-center fixed top-0 bottom-0 left-0 right-0 z-10`}
+      }  backdrop-blur-md flex justify-center items-center fixed top-0 bottom-0 left-0 right-0 z-10 p-4`}
       onClick={onClickClose}
     >
-      <div className="bg-white p-10 ">{children}</div>
+      <div className="flex flex-col rounded-lg  shadow-2xl bg-white overflow-hidden w-full min-w-[200px] md:max-w-[420px] md:w-auto">
+        <div className="flex flex-row justify-end p-3">
+          <button className="icon-button-style" onClick={onClickClose}>
+            <FiXCircle size={20} />
+          </button>
+        </div>
+        <div className="flex flex-col p-6 md:p-8">{children}</div>
+      </div>
     </div>
   );
 };
