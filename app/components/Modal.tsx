fd3d73d7f0b7cf728f1cd994edb7d5b12acd1d6f
soohyn <sooheyonjo@gmail.com>
2024-06-07 18:33:52 +0900

    modal hidden, fixed event bubble

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, HtmlHTMLAttributes, useEffect } from "react";
+import { FC, HtmlHTMLAttributes, MouseEvent, useEffect } from "react";
 import { FiXCircle } from "react-icons/fi";
 
 interface ModalProps extends HtmlHTMLAttributes<HTMLDivElement> {
@@ -31,17 +31,20 @@ const Modal: FC<ModalProps> = ({ isOpened, children, onClose }) => {
   return (
     <div
       className={`${
-        isOpened ? "visible" : "invisible"
+        isOpened ? "" : "hidden"
       }  backdrop-blur-md flex justify-center items-center fixed top-0 bottom-0 left-0 right-0 z-10 p-4`}
       onClick={onClickClose}
     >
-      <div className="flex flex-col rounded-lg  shadow-2xl bg-white overflow-hidden w-full min-w-[200px] md:max-w-[420px] md:w-auto">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="flex flex-col rounded-lg  shadow-2xl bg-white overflow-hidden w-full min-w-[300px] max-w-[400px] md:max-w-[440px] md:w-auto"
+      >
         <div className="flex flex-row justify-end p-3">
           <button className="icon-button-style" onClick={onClickClose}>
             <FiXCircle size={20} />
           </button>
         </div>
-        <div className="flex flex-col p-6 md:p-8">{children}</div>
+        <div className="flex flex-col p-5 md:p-6">{children}</div>
       </div>
     </div>
   );
