b9fda796b99edf2fda283fdc86312f2f6d148554
soohyn <sooheyonjo@gmail.com>
2024-06-10 23:54:06 +0900

    update guest

--- app/api/guest/[id]/route.ts
+++ app/api/guest/[id]/route.ts
@@ -23,7 +23,7 @@ export const DELETE = async (
       return NextResponse.json({ message: "Not exist guest" }, { status: 400 });
     }
 
-   const hashedPassword = existGuestResponse.data.password
+    const hashedPassword = existGuestResponse.data.password;
 
     if (hashedPassword !== password) {
       return NextResponse.json(
@@ -31,17 +31,55 @@ export const DELETE = async (
         { status: 400 }
       );
     }
-    
+
     const response = await supabaseClient.from("guest").delete().eq("id", id);
 
     if (response.error) {
       throw "not delete";
     }
 
-    return NextResponse.json({ params }, { status: 200 });
+    return NextResponse.json(response);
   } catch (error) {
     console.error(error);
 
     return NextResponse.json({ message: "Server Error" }, { status: 500 });
+  }
+};
+
+export const PUT = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { message } = await req.json();
+    const { id } = params;
+
+    if (!id || !message) {
+      return NextResponse.json({ message: "Not exist data" }, { status: 400 });
+    }
+
+    const existGuestResponse = await supabaseClient
+      .from("guest")
+      .select()
+      .eq("id", id)
+      .limit(1)
+      .single();
+
+    if (!existGuestResponse.data) {
+      return NextResponse.json({ message: "Not exist guest" }, { status: 400 });
+    }
+
+    const response = await supabaseClient
+      .from("guest")
+      .update({ message })
+      .eq("id", id)
+      .select()
+      .single();
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
   }
 };
