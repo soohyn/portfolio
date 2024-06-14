3bab17639a7fa96eac3d13d9b4f456ef715fa1f8
soohyn <sooheyonjo@gmail.com>
2024-06-15 08:05:31 +0900

    update footer contacts

--- app/components/Contacts.tsx
+++ app/components/Contacts.tsx
@@ -1,15 +1,18 @@
-import { GITHUB_URL, LINKEDIN_URL } from "@/public/config";
+import { GITHUB_URL, GMAIL_ADDR, LINKEDIN_URL } from "@/public/config";
 import { FC } from "react";
-import { SiGithub, SiLinkedin } from "react-icons/si";
+import { SiGithub, SiGmail, SiLinkedin } from "react-icons/si";
 
 const Contacts: FC = () => {
   return (
     <div className="flex flex-row gap-3">
-      <a target="__blank" href={GITHUB_URL} className="icon-button-style cursor-pointer">
+      <a target="_blank" href={GITHUB_URL} className="icon-button-style cursor-pointer">
         <SiGithub size={24} />
       </a>
-      <a target="__blank" href={LINKEDIN_URL} className="icon-button-style cursor-pointer">
+      <a target="_blank" href={LINKEDIN_URL} className="icon-button-style cursor-pointer">
         <SiLinkedin size={24} />
+      </a>
+      <a target="_blank" href={`mailto:${GMAIL_ADDR}`} className="icon-button-style cursor-pointer">
+        <SiGmail size={24} />
       </a>
     </div>
   );
