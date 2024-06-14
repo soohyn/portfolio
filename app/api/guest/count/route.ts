92535b1a6e9134489ef31556bbda7dfda02791c6
soohyn <sooheyonjo@gmail.com>
2024-06-14 23:41:52 +0900

    todo check why this change result

--- app/api/guest/count/route.ts
+++ app/api/guest/count/route.ts
@@ -4,19 +4,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
+    const { searchParams } = new URL(req.url);
+    const pageText = searchParams.get("page");
+
+    if (!pageText) {
+      return NextResponse.json({ mssage: "Not exist page" }, { status: 200 });
+    }
+
     const response = await supabaseClient
       .from("guest")
       .select()
       .order("created_at", { ascending: false });
 
-    if (!response.data) {
-      throw "no data";
+    const guests = response.data ?? [];
+
+    if (!guests) {
+      throw "no guests";
     }
 
-    const guestCount = response.data.length;
-    const pageCount = guestCount > 1 ? Math.ceil(guestCount / PAGE_LIMIT) : 1;
+    const pageCount = guests.length > 1 ? guests.length / PAGE_LIMIT : 1;
 
-    return NextResponse.json({ pageCount });
+    return NextResponse.json({ pageCount }, { status: 200 });
   } catch (error) {
     console.error(error);
 
